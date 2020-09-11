ac2a813047a3437d3306d6fe6f1cbd8d6e49598e
angeloarcillas <angeloarcillas512@gmail.com>
2020-09-11 19:10:51 +0800

    finish tweety laravel series

---
+++ old/laravel/Tweety/database/seeds/DatabaseSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+use App\Tweet;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // $this->call(UserSeeder::class);
+        factory(User::class, 10)->create();
+        factory(Tweet::class, 30)->create();
+    }
+}
---
+++ old/laravel/Tweety/resources/views/tweets/index.blade.php
@@ -0,0 +1,7 @@
+<x-app>
+  <div>
+    @include ('includes/_publish-tweet-panel')
+
+    @include ('includes/_timeline')
+  </div>
+</x-app>
