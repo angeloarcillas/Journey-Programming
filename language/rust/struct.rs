5eba1d62b6ab34dcbf813453378848e014dd9c92
Angelo Arcillas <angeloarcillas512@gmail.com>
2021-03-23 15:18:27 +0800

    add struct

---
+++ language/rust/struct.rs
@@ -0,0 +1,11 @@
+
+// regular struct
+// group of variables that can be accessed  via a single pointer
+struct Color {
+  blue: i8,
+  yellow: f32,
+  red: String,
+}
+
+// tuple struct
+struct Tuple(u32, String);
