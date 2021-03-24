3d6f7c2ab69a447bd69e7c99d2e3ed5f9c525efd
Angelo Arcillas <angeloarcillas512@gmail.com>
2021-03-24 15:40:43 +0800

    add loops

---
+++ language/python/loops.py
@@ -0,0 +1,18 @@
+from typing import Sequence
+
+
+expression = True
+
+
+# while loop
+while expression:
+  print("code...")
+
+# for loop
+sequence = range(0, 5)
+for x in sequence:
+  print(x)
+
+break # stop the current loop
+continue # skip the rest of the current iteration then continue next iteration
+pass
