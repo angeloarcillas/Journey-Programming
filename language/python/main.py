d2770b2f0534ba4546a3041ecee392561d99132f
Angelo Arcillas <angeloarcillas512@gmail.com>
2021-03-24 14:30:07 +0800

    :zap: wip

---
+++ language/python/main.py
@@ -0,0 +1,15 @@
+# output
+foo = "Hello"
+bar = "World"
+
+print('Hello, World!')
+print(foo, bar, "!") # Hello World !
+
+# input
+my_input = input()
+print(my_input)
+
+# python variables
+foo = "String"
+bar = 55
+baz = True
