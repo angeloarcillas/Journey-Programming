32c8718256b9641aafe6a9c20dca09a564c9834c
Angelo Arcillas <angeloarcillas512@gmail.com>
2021-03-24 14:42:16 +0800

    add operator

---
+++ language/python/operator.py
@@ -0,0 +1,20 @@
+# basic math
+addition = 5 + 55
+subtraction = 5 - 5
+multiplication = 5 * 5
+division = 14 / 3
+modulus = 13 % 5
+quotient = 14 // 3
+exponential = 2 ** 5 # 2^5
+
+# concat string
+foo = "Hello"+ " " +"World" + "!" # Hello World!
+
+# multiply string
+bar = "a" * 3 # aaa
+
+# multi assignment
+a, b, c = 1, 2, 3
+
+# same value to multiple variable
+a = b = c = 5
