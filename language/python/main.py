5eb73241596b7eb6e31c307209ce76a7ded0b786
Angelo Arcillas <angeloarcillas512@gmail.com>
2021-03-24 16:23:52 +0800

    add end='' on print()

--- language/python/main.py
+++ language/python/main.py
@@ -5,6 +5,9 @@ bar = "World"
 print('Hello, World!')
 print(foo, bar, "!") # Hello World !
 
+# change end of line behavior
+print("alter end of line", end=" ")
+
 # input
 my_input = input()
 print(my_input)
